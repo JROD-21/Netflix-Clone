525084eb3223a2755f1714cfb99bf35f22e92f48
JROD-21 <joseph.f.rodas@gmail.com>
2023-01-26 12:52:46 -0500

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Developer J-ROD on 1/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
